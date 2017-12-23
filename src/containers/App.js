aa93ca92d7232d7d76a0bf88e68368bbdd7d9f8a
Munchic <khoi.u.pham@gmail.com>
2017-12-23 12:46:31 -0800

    Create a React app and publish it to GitHub Pages

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Khoi Pham</h1>
+          <h2>Computer Science Student at Minerva Schools at KGI</h2>
+          <h3>Class of 2021</h3>
+        </header>
+        <p className="App-intro">
+          Just testing out the website lol
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
